f0a29817782f49ff902a90deca3fb002f8d52455
Ricardo Franco <r_franco84@yahoo.com>
2020-10-10 15:51:03 -0700

    Initial Unity files and debug assets.

---
+++ TreasureHunter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+//using System.Diagnostics;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float acceleration;
+    public float maxSpeed;
+    public Rigidbody2D rb;
+    public bool faceRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // receive input. 
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        // Horizontal Movement
+        float xVel = Input.GetAxis("Horizontal") * acceleration * Time.smoothDeltaTime + +rb.velocity.x;
+        if(xVel>maxSpeed||xVel<maxSpeed*-1)
+        {
+            xVel = maxSpeed*Input.GetAxis("Horizontal");
+        }
+        if((faceRight && Input.GetAxis("Horizontal") <0) ||(!faceRight && Input.GetAxis("Horizontal")>0))
+         {
+            flip();
+        }
+        
+        // Implement Jump
+
+        float yVel = rb.velocity.y;
+        rb.velocity = new Vector2(xVel, yVel);
+
+        
+    }
+
+    void flip()
+    {
+        // Make character face different direction.
+    }
+}
