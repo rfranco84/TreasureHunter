87527ab147ea396f544e4dfd58071558bd62cdf0
Ricardo Franco <r_franco84@yahoo.com>
2020-10-15 22:09:52 -0700

    Beginnings of walking movement

--- TreasureHunter/Assets/Scripts/PlayerController.cs
+++ TreasureHunter/Assets/Scripts/PlayerController.cs
@@ -13,7 +13,8 @@ public class PlayerController : MonoBehaviour
     public float maxSpeed;
     public Rigidbody2D rb;
     public bool faceRight;
-
+    private bool buttonPressed;
+    private bool jumpButton;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,6 +30,7 @@ public class PlayerController : MonoBehaviour
 
     void PlayerMove()
     {
+
         // Horizontal Movement
         float xVel = Input.GetAxis("Horizontal") * acceleration * Time.smoothDeltaTime + +rb.velocity.x;
         if(xVel>maxSpeed||xVel<maxSpeed*-1)
@@ -39,13 +41,39 @@ public class PlayerController : MonoBehaviour
          {
             flip();
         }
-        
+
         // Implement Jump
+        // Check if grounded.
+        jumpButton = Input.GetButton("Jump");
+        if (jumpButton)
+            buttonPressed = true;
+
+        if(isGrounded())
+        {
+            // Can jump if jump button hasn't been held from previous frames.
+            if (!jumpButton)
+                buttonPressed = false;
+            
+        }
 
         float yVel = rb.velocity.y;
+        
         rb.velocity = new Vector2(xVel, yVel);
 
         
+    }
+
+    bool isGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(transform.position.x, transform.position.y-gameObject.GetComponent<CapsuleCollider2D>().size.y/2-0.01f), Vector2.down, 0.02f);
+        if(hit.collider!=null)
+        {
+            if(hit.collider.gameObject.tag.Equals("Ground"))
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
     void flip()
